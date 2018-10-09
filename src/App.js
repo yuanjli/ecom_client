8a2581bddf4dc61d54512ff2913905c3d1d4a2c9
yuanjli <liyuanjienetwork@gmail.com>
2018-10-09 15:14:40 -0700

    added nav and layouts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+
 class App extends Component {
   render() {
     return (
