57b4a77c7a56fef6f47e69dd3c429a33b5f0e232
yuanjli <liyuanjienetwork@gmail.com>
2018-10-18 00:04:25 -0700

    setting up product detail page

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -3,29 +3,14 @@ import PageTop from '../utils/page_top';
 
 
 import { connect } from 'react-redux';
-import { addToCart } from '../../actions/user_actions';
 import { getProductDetail, clearProductDetail } from '../../actions/products_actions';
 
 
 class Product extends Component {
-     componentDidMount(){
-        const id = this.props.match.params.id;
-        this.props.dispatch(getProductDetail(id)).then(()=>{
-            if(!this.props.products.prodDetail){
-                this.props.history.push('/');
-            }
-        })
-    }
-
-    componentWillUnmount(){
-        this.props.dispatch(clearProductDetail())
-    }
+     
 
 
-    addToCartHandler(id){
-        this.props.dispatch(addToCart(id))
-    }
-    
+
     render() {
         return (
             <div>
@@ -33,24 +18,12 @@ class Product extends Component {
                     title="Product detail"
                 />
                 <div className="container">
-                {
-                    this.props.products.prodDetail ?
-                    <div className="product_detail_wrapper">
-                        <div className="left">
-                            <div style={{width:'500px'}}>
-                              
-                                    detail={this.props.products.prodDetail}
-                               
-                            </div>
-                        </div>
-                        <div className="right">
-                           
-                             detail={this.props.products.prodDetail}
-                            
-                        </div>
-                    </div>
-                    : 'Loading'
-                }
+                    <h1> product page here </h1>
+
+                </div>
+                <div className="container">
+                    <h2>Product Page!</h2>
+                    <h3> name: {this.props.user.name} </h3>
 
                 </div>                
             </div>
