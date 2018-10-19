3bde4abefe5df8d71ca740762640f4357f7379d3
yuanjli <liyuanjienetwork@gmail.com>
2018-10-18 23:59:33 -0700

    adding product detail page

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import PageTop from '../utils/page_top';
 
+import ProdNfo from './prodNfo';
 
-import { connect } from 'react-redux';
+import { connect } from 'react-redux';  // when we get the ID of the product, we need to fetch the information
 import { getProductDetail, clearProductDetail } from '../../actions/products_actions';
+
 
 
 class Product extends Component {
      
+     // fetch the information
+    componentDidMount(){
+        const id = this.props.match.params.id;
+        console.log(id)
+        // checked for getting the id correctly. 
+        this.props.dispatch(getProductDetail(id))
 
+    }
+
+    componentWillUnmount(){
+        this.props.dispatch(clearProductDetail())
+    }
 
 
     render() {
@@ -19,6 +32,22 @@ class Product extends Component {
                 />
                 <div className="container">
                     <h1> product page here </h1>
+                {
+                    this.props.products.prodDetail ?
+                    <div className="product_detail_wrapper">
+                        <div className="left">
+                            images
+                        </div>
+                        <div className="right">
+                            <ProdNfo
+                                addToCard={(id)=> this.addToCartHandler(id)}
+                                detail={this.props.products.prodDetail}
+                            />
+                        </div>
+                    </div>
+
+                    :'Loading'
+                }    
 
                 </div>
                 <div className="container">
@@ -37,4 +66,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default Product;
+export default connect(mapStateToProps)(Product);
