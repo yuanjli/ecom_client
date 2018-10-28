e1663bd3abca3986c3fbb706d5d91453badf0c59
yuanjli <liyuanjienetwork@gmail.com>
2018-10-27 22:13:10 -0700

    adding lightbox and prodimage component

---
+++ src/components/Product/prodImg.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class ProdImg extends Component {
+
+
+
+	render() {
+		return (
+			<div>
+
+
+			</div>
+			);
+	}
+
+
+}
+
+
+export default ProdImg;
---
+++ src/components/utils/lightbox.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Lightbox from 'react-images';
+
+class ImageLightBox extends Component {
+
+	state = {
+		lightboxIsOpen: true,
+		currentImage: this.props.pos,
+		images: []
+	}
+
+	render() {
+		return (
+			<Lightbox 
+
+
+			/>);
+
+	}
+
+
+}
+
+
+export default ImageLightBox;
