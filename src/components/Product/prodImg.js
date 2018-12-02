8caebe02e5e53487e43bc96920e336fa11ac8211
yuanjli <liyuanjienetwork@gmail.com>
2018-12-01 23:19:42 -0800

    created state to the product image page

--- src/components/Product/prodImg.js
+++ src/components/Product/prodImg.js
@@ -3,9 +3,30 @@ import React, { Component } from 'react';
 
 class ProdImg extends Component {
 
+	state = {
+		lightbox: false,
+		imagePos: 0,
+		lightboxImages: []
+	}
+
+
+	componentDidMount(){
+		if(this.props.detail.images.length > 0){
+			let lightboxImages = [];
+
+			this.props.detail.images.forEach(item =>{
+				lightboxImages.push(item.url)
+			})
+
+			this.setState({
+				lightboxImages
+			})
+		}
+	}
 
 
 	render() {
+		// console.log(this.state)
 		return (
 			<div>
 
