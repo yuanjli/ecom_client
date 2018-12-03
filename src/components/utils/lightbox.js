a93ffbb7e588736e8e4a601b1d7a95ed5434795a
yuanjli <liyuanjienetwork@gmail.com>
2018-12-03 00:08:22 -0800

    added lightbox

--- src/components/utils/lightbox.js
+++ src/components/utils/lightbox.js
@@ -9,13 +9,39 @@ class ImageLightBox extends Component {
 		images: []
 	}
 
+// Documentation for react Images
+// https://github.com/jossmac/react-images
+	
+	static getDerivedStateFromProps(props, state){
+		if(props.images){
+			const images = [];
+			props.images.forEach(element=>{
+				images.push({src:`${element}`})
+			});
+			return state = {
+				images 
+			}
+		}
+		return false
+	}
+
+	closeLightbox = () => {
+		this.props.onclose();
+	}
+
+
 
 
 
 	render() {
 		return (
 			<Lightbox 
-
+				currentImage={this.state.currentImage}
+				images={this.state.images}
+				isOpen={this.state.lightboxIsOpen}
+				onClickPrev={() => this.gotoPrevious()}
+				onClickNext={() => this.gotoNext()}
+				onClose={() => this.closeLightbox()}
 
 			/>);
 
